a15e498d390246d2348bc8e8817e937f8ac71245
ThangNguyen157 <86416687+ThangNguyen157@users.noreply.github.com>
2024-08-08 18:31:14 -0400

    fixn wrong datatype OpenCV uses np.uint8 datatype, so I need to change to that datatype to avoid unwated negative number

--- code/create_input.py
+++ code/create_input.py
@@ -1,11 +1,14 @@
+###########
+
+# this file is used to create the input to train the model
+
+###########
+
 import cv2
 import os
 from dotenv import load_dotenv
 import numpy as np
 import mediapipe as mp
-from mediapipe.tasks.python import vision
-import time
-import math
 
 def create_input_file(name, path):
     currentpath = os.getcwd()
@@ -21,30 +24,43 @@ def create_input_file(name, path):
     base_options=BaseOptions(model_asset_path=os.getenv('modelPath')),
     running_mode=VisionRunningMode.IMAGE)
 
+    landmarker = HandLandmarker.create_from_options(options)
+
     # loop through each folder in the parent folder
     for dir in featuresdir:
         # loop through each iamge file in each alphabet folder in the parent folder
         files = os.listdir(currentpath + path + '\\' + dir)
         # need to create a fixed size numpy first because it will be faster than appending
-        featuresArray = np.zeros((len(files), 190, 190, 3)) # if want for grayscale iamge then remove 3
+        # dtype must be np.uint8 because opencv us that format for images. or else would make turn numbers into some weird negative numbers
+        featuresArray = np.zeros((len(files), 190, 190), dtype=np.uint8) # add 3 to the end if RGB image
         # labelsArray = np.empty(len(files), dtype=str)
         n = 0
         for file in files:
             img = cv2.imread(currentpath + path + '\\' + dir + '\\' + file, cv2.IMREAD_COLOR) # load in an imamge in BGR format as default in opencv
+            
+            if img is None:
+                featuresArray = featuresArray[:-1]
+                # labelsArray = labelsArray[:-1]
+                continue
             # cv2.imshow('Image', img)
             # cv2.waitKey(0)
             # cv2.destroyAllWindows()
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # convert to RGB format
 
             mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=img)
-            with HandLandmarker.create_from_options(options) as landmarker:
-                landmarker_result = landmarker.detect(mp_image)
 
+            # detect the hand
+            
+            landmarker_result = landmarker.detect(mp_image)
+
+            # handle the case when can not detect hand in the image
             if not landmarker_result or not landmarker_result.hand_landmarks:
+                #time.sleep(1000000)
                 featuresArray = featuresArray[:-1]
-                labelsArray = labelsArray[:-1]
+                # labelsArray = labelsArray[:-1]
                 continue
-            # get the coner for the bounding box of the hand by finding
+
+            # get the corners for the bounding box of the hand by finding
             # bigest and smallest x and y coordinate of the landmark
             for i in range(len(landmarker_result.hand_landmarks[0])):
                 #img = cv2.circle(img, (round(landmarker_result.hand_landmarks[0][i].x*img.shape[1]), 
@@ -73,21 +89,20 @@ def create_input_file(name, path):
             max_x = round((max_x + 0.017) * img.shape[1])
             
             hand = img[min_y:max_y, min_x:max_x]
+
+            # handle the problem when the image cropped is empty
+            if hand is None or hand.shape[0] == 0 or hand.shape[1] == 0:
+                featuresArray = featuresArray[:-1]
+                # labelsArray = labelsArray[:-1]
+                continue
+
+            hand = cv2.cvtColor(hand, cv2.COLOR_RGB2GRAY)
             hand = cv2.resize(hand, (190,190))
-            print(hand.shape)
-            cv2.imshow('Image', hand)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
 
             # print(landmarker_result.hand_landmarks[0][0].x)
             featuresArray[n] = hand
-            # labelsArray[n] = dir
             n+=1
-            # time.sleep(100000000)
-
-
-            # will resize hand to 250x250
-            # creaate npy file for each alphabet folder.
+        
         if path == os.getenv('trainPath'):
             np.save(currentpath + '\\processed_data' + '\\data_for_training' + '\\' + dir + '.npy', featuresArray)
         elif path == os.getenv('testPath'):
@@ -97,7 +112,7 @@ def create_input_file(name, path):
 
 def main():
     load_dotenv()
-    create_input_file('training', os.getenv('trainPath'))
+    create_input_file('testing', os.getenv('trainPath'))
     create_input_file('testing', os.getenv('testPath'))
     create_input_file('testing', os.getenv('validationPath'))
 
